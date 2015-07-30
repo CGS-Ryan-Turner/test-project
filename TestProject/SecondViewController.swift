06ba1fbf91d7e48160e23d3aae9463ce5502b216
Ryan Turner <1479@cgs.act.edu.au>
2015-07-30 15:08:32 +1000

    20: More github testing

--- TestProject/SecondViewController.swift
+++ TestProject/SecondViewController.swift
@@ -6,4 +6,21 @@
 //  Copyright (c) 2015 Apress. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        println("Hi, intelligent person");
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
